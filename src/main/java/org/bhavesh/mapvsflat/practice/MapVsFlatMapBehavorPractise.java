8ae63efc2e2180c32d85fd8672eab5fec453f049
bhavesh-zanane-2911 <bhaveshzanzane2911@outlook.com>
2024-05-12 11:47:29 +0530

    Bhavesh:Created a base to start studying map vs flat map behavior

---
+++ src/main/java/org/bhavesh/mapvsflat/practice/MapVsFlatMapBehavorPractise.java
@@ -0,0 +1,11 @@
+package org.bhavesh.mapvsflat.practice;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MapVsFlatMapBehavorPractise {
+
+    private static final Logger LOG = LoggerFactory.getLogger(MapVsFlatMapBehavorPractise.class);
+
+
+}
