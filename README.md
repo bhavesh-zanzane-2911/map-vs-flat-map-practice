fb03eff8f727431b69181eff59e9bee8e9f3fc9f
bhavesh-zanane-2911 <bhaveshzanzane2911@outlook.com>
2024-05-13 17:44:05 +0530

    Bhavesh:Updated Readme.md file

--- README.md
+++ README.md
@@ -1,1 +1,58 @@
-# map-vs-flat-map-practice
+# Map vs FlatMap Behavior Practice
+
+This project includes a set of methods for practicing the behavior of `map` and `flatMap` operations in Java streams. It contains methods to retrieve information about persons, such as their Aadhar number, work experience, and the companies they have worked for.
+
+## Methods
+
+### 1. `getPersonByName`
+
+- **Description**: Retrieves a person by their name from the list of persons.
+- **Parameters**:
+    - `name` (String): The name of the person to retrieve.
+- **Returns**:
+    - `Person`: The person object with the specified name.
+- **Throws**:
+    - `RuntimeException`: If no person with the specified name is found.
+
+### 2. `getAddharNumberByName`
+
+- **Description**: Retrieves the Aadhar number of a person by their name from the list of persons.
+- **Parameters**:
+    - `name` (String): The name of the person whose Aadhar number is to be retrieved.
+- **Returns**:
+    - `Integer`: The Aadhar number of the person with the specified name.
+- **Throws**:
+    - `RuntimeException`: If no person with the specified name is found or if the Aadhar number is not available.
+
+### 3. `getExperienceByNameUsingMap`
+
+- **Description**: Retrieves the total work experience of a person by their name using map operation.
+- **Parameters**:
+    - `name` (String): The name of the person whose work experience is to be retrieved.
+- **Returns**:
+    - `List<List<EmployeeExperience>>`: A list of lists containing work experiences of the person with the specified name. If no person with the specified name is found, an empty list is returned.
+
+### 4. `getExperienceByNameUsingFlatMap`
+
+- **Description**: Retrieves the total work experience of a person by their name using flatMap operation.
+- **Parameters**:
+    - `name` (String): The name of the person whose work experience is to be retrieved.
+- **Returns**:
+    - `List<EmployeeExperience>`: A list of work experiences of the person with the specified name. If no person with the specified name is found, an empty list is returned.
+
+### 5. `getCompaniesByNameUsingFlatMap`
+
+- **Description**: Retrieves the names of companies where a person has worked by their name using flatMap operation.
+- **Parameters**:
+    - `name` (String): The name of the person whose companies' names are to be retrieved.
+- **Returns**:
+    - `List<String>`: A list of company names where the person with the specified name has worked. If no person with the specified name is found, an empty list is returned.
+
+## Unit Tests
+
+The project also includes unit tests for each method to ensure their correctness and functionality.
+
+## Running Tests
+
+To run the tests, execute the `MapVsFlatMapBehavorPractiseTest` class.
+
--- src/main/java/org/bhavesh/mapvsflat/practice/MapVsFlatMapBehavorPractise.java
+++ src/main/java/org/bhavesh/mapvsflat/practice/MapVsFlatMapBehavorPractise.java
@@ -8,61 +8,115 @@ import org.slf4j.LoggerFactory;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Stream;
 
 public class MapVsFlatMapBehavorPractise {
 
     private static final Logger LOG = LoggerFactory.getLogger(MapVsFlatMapBehavorPractise.class);
 
-    public static Person getPersonByName(String name){
+     /**
+     * Retrieves a person by their name from the list of persons.
+     *
+     * @param name The name of the person to retrieve.
+     * @return The person object with the specified name.
+     * @throws RuntimeException If no person with the specified name is found.
+     */
+    public static Person getPersonByName(String name) {
+        // Retrieve the list of persons
         List<Person> personsList = PersonService.getPersonsList();
-        //filter out companies for which Bhavesh has worked
+
+        // Filter out persons with the specified name
         Optional<Person> firstPerson = personsList.stream()
                 .filter(person -> name.equals(person.getName()))
                 .findFirst();
+
+        // Return the first person found with the specified name, or throw an exception if not found
         return firstPerson.orElseThrow(RuntimeException::new);
     }
 
-    public static Integer getAddharNumberByName(String name){
+
+    /**
+     * Retrieves the Aadhar number of a person by their name from the list of persons.
+     *
+     * @param name The name of the person whose Aadhar number is to be retrieved.
+     * @return The Aadhar number of the person with the specified name.
+     * @throws RuntimeException If no person with the specified name is found or if the Aadhar number is not available.
+     */
+    public static Integer getAddharNumberByName(String name) {
+        // Retrieve the list of persons
         List<Person> personsList = PersonService.getPersonsList();
-        //filter out companies for which Bhavesh has worked
+
+        // Filter out persons with the specified name and retrieve their Aadhar numbers
         Optional<Integer> addharNumberOptional = personsList.stream()
                 .filter(person -> name.equals(person.getName()))
                 .map(Person::getAddharNo)
                 .findFirst();
+
+        // Return the Aadhar number of the first person found with the specified name, or throw an exception if not found or Aadhar number is not available
         return addharNumberOptional.orElseThrow(RuntimeException::new);
     }
 
-    public static List<List<EmployeeExperience>> getExperienceByNameUsingMap(String name){
+
+    /**
+     * Retrieves the total work experience of a person by their name using map operation.
+     *
+     * @param name The name of the person whose work experience is to be retrieved.
+     * @return A list of lists containing work experiences of the person with the specified name.
+     *         If no person with the specified name is found, an empty list is returned.
+     */
+    public static List<List<EmployeeExperience>> getExperienceByNameUsingMap(String name) {
+        // Retrieve the list of persons
         List<Person> personsList = PersonService.getPersonsList();
-        //filter out companies for which Bhavesh has worked using Map
+
+        // Filter out persons with the specified name and map their total experience
+        // map operation extracts the total experience of each person
         return personsList.stream()
                 .filter(person -> name.equals(person.getName()))
                 .map(Person::getTotalExperience)
                 .toList();
     }
 
-    public static List<EmployeeExperience> getExperienceByNameUsingFlatMap(String name){
+
+    /**
+     * Retrieves the total work experience of a person by their name using flatMap operation.
+     *
+     * @param name The name of the person whose work experience is to be retrieved.
+     * @return A list of work experiences of the person with the specified name.
+     *         If no person with the specified name is found, an empty list is returned.
+     */
+    public static List<EmployeeExperience> getExperienceByNameUsingFlatMap(String name) {
+        // Retrieve the list of persons
         List<Person> personsList = PersonService.getPersonsList();
-        //filter out companies for which Bhavesh has worked using Flat Map
-        //flat map takes stream as a input
-        return  personsList.stream()
+
+        // Filter out persons with the specified name and flatMap their total experience
+        // flatMap takes a stream as input
+        return personsList.stream()
                 .filter(person -> name.equals(person.getName()))
                 .flatMap(person -> person.getTotalExperience().stream())
                 .toList();
-
     }
 
-    public static List<String> getCompaniesByNameUsingFlatMap(String name){
+
+    /**
+     * Retrieves the names of companies where a person has worked by their name using flatMap operation.
+     *
+     * @param name The name of the person whose companies' names are to be retrieved.
+     * @return A list of company names where the person with the specified name has worked.
+     *         If no person with the specified name is found, an empty list is returned.
+     */
+    public static List<String> getCompaniesByNameUsingFlatMap(String name) {
+        // Retrieve the list of persons
         List<Person> personsList = PersonService.getPersonsList();
-        //filter out companies for which Bhavesh has worked using Flat Map
+
+        // Filter out persons with the specified name, flatMap their total experience,
+        // and map their company names
+        // flatMap takes a stream as input
         return personsList.stream()
                 .filter(person -> name.equals(person.getName()))
                 .flatMap(person -> person.getTotalExperience().stream())
                 .map(EmployeeExperience::getCompanyName)
                 .toList();
-
     }
+
 
 
 
